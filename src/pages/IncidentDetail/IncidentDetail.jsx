575110fbaf6aba3aa814568b386c7b2a7973d010
nuinn <dilley.marc@gmail.com>
2024-04-02 20:18:09 +0200

    started work on IncidentDetail

---
+++ src/pages/IncidentDetail/IncidentDetail.jsx
@@ -0,0 +1,12 @@
+import { useParams } from 'react-router-dom'
+
+function IncidentDetail(props) {
+  const { incident_id } = useParams()
+  return (
+    <>
+      <h1>TEst</h1>
+    </>
+  )
+}
+
+export default IncidentDetail
